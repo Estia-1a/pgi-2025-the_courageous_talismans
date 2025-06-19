79c82a7ba8c93a6b6e0e686393228860afa6ac1a
Arthur2733 <arthur.moricet@etu.estia.fr>
2025-06-19 10:58:00 +0200

    min_component ok

--- src/features.c
+++ src/features.c
@@ -82,43 +82,6 @@ void dimension (char *source_path) {
 
 }
 
-    void min_component(char *filename, char component) {
-        unsigned char *data = NULL;
-        int width = 0, height = 0, channel_count = 0;
-
-    read_image_data(filename, &data, &width, &height, &channel_count);
-
-
-    int max_value = -1;
-    int max_x = -1, max_y = -1;
-
-    int c_index = 0;
-    if (component == 'R') c_index = 0;
-    else if (component == 'G') c_index = 1;
-    else if (component == 'B') c_index = 2;
-    else {
-        fprintf(stderr, "Composante invalide : %c (utiliser R, G ou B)\n", component);
-        return;
-    }
-
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            int index = (y * width + x) * channel_count;
-            if (channel_count < 3) continue; 
-
-            int value = data[index + c_index];
-            if (value > max_value) {
-                max_value = value;
-                max_x = x;
-                max_y = y;
-            }
-        }
-    }
-
-    if (max_value >= 0)
-        printf("max_component %c (%d, %d): %d\n", component, max_x, max_y, max_value);
-}
-
 void rotate_cw(const char *filename){
     unsigned char *data = NULL;
     int width, height, n;
